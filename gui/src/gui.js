437bd0f22ecdc915ae287a4a70286bfc482cd88a
SasPes <saspes@gmail.com>
2017-03-09 20:05:40 +0100

    Init commit

---
+++ gui/src/gui.js
@@ -0,0 +1,46 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Table = React.createClass({
+    render: function render() {
+        var _self = this;
+
+        var thead = React.DOM.thead({},
+                React.DOM.tr({},
+                        this.props.cols.map(function (col) {
+                            return React.DOM.th({}, col);
+                        })));
+
+        var tbody = this.props.rows.map(function (row) {
+            return React.DOM.tr({},
+                    _self.props.cols.map(function (col) {
+                        return React.DOM.td({}, row[col] || "");
+                    }));
+        });
+
+        return React.DOM.table({}, [thead, tbody]);
+    }
+
+});
+
+var container = document.getElementById('example');
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+var users = httpGet("http://localhost:8080/api/user/list").replace(/true/g, '"true"').replace(/false/g, '"false"');
+users = JSON.parse(users);
+
+var tableModel = {
+    cols: ["id", "username", "password", "accountNonExpired", "enabled", "accountNonLocked", "credentialsNonExpired"],
+    rows: users
+};
+
+ReactDOM.render(React.createElement(Table, tableModel), container);
+
+document.getElementsByTagName("table")[0].classList.add("table");
---
+++ gui/webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+    'react': 'React',
+    entry: APP_DIR + '/gui.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module : {
+        loaders : [
+            {
+                test : /\.js?/,
+                include : APP_DIR,
+                loader : 'babel'
+            }
+        ]
+    }
+};
+
+module.exports = config;
